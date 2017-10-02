f160d0e069056ef4b2e9c64bbd91bab79c2b117f
Sierra Clibourne <clibo100@mail.chapman.edu>
2017-10-02 14:49:57 -0700

    added class Card

---
+++ CardShuffle/app/src/main/java/edu/chapman/cardshuffle/Card.java
@@ -0,0 +1,32 @@
+package edu.chapman.cardshuffle;
+
+/**
+ * Created by Sea Air AHHHHHHHH on 10/2/2017.
+ */
+
+public class Card
+{
+    String suit;
+    int number;
+
+    public Card(String suit, int number) {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
